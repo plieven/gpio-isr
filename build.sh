025f61d67816700510380cb76a8dc756506c4178
Peter Lieven <pl@kamp.de>
2017-10-06 09:54:30 +0200

    build: check for binaries and adjust messages
    Signed-off-by: Peter Lieven <pl@kamp.de>

--- build.sh
+++ build.sh
@@ -1,5 +1,15 @@
 #!/bin/bash -e
 
+echo Checking for required binaries...
+for BIN in pigs timeout curl; do
+ which $BIN >/dev/null && true
+ if [ $? -ne 0 ]; then
+  echo ERR: required binary $BIN not found in PATH
+  exit 1
+ fi
+done
+
+echo
 echo Building Target...
 gcc -o gpio-isr gpio-isr.c -lpigpio -lrt -Wall -Werror
 
@@ -30,11 +40,8 @@ echo
 echo You may want to modify the GPIO pins that are monitored by editing:
 echo /etc/systemd/system/gpio-isr.service
 echo
-echo You can find a listing of all pins with the command: gpio readall
-echo gpio-isr expects the wiringPi pin id.
-echo
 echo You can run gpio-isr in monitor mode to check if you receive interrupts:
-echo ./gpio-isr -m -p 0 -p 3
+echo ./gpio-isr -m -U -p 17
 echo
 echo You may preseed the interrupt totalCount for a pin by editing the files
 echo in /var/lib/gpio-isr/pinN.totalCount when the application is stopped.
